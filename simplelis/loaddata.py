5c829e6cf002910110cb291a3a69bdf67897d63f
Likit Preeyanon <likit.pre@mahidol.edu>
2018-08-10 00:57:20 +0700

    added datafiles for simplelis
    added migration files
    
    added functions to load more data into db
    
    renamed column names with snakecase

--- simplelis/loaddata.py
+++ simplelis/loaddata.py
@@ -1,10 +1,11 @@
 import os
 import glob
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
+from random import randint
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from models import Patient
+from models import Patient, Test, Order, Lab
 
 data_dir = 'datafiles'
 
@@ -14,22 +15,92 @@ engine = create_engine('postgres+psycopg2://postgres:{}@pg/simplelis'.
 Session = sessionmaker(bind=engine)
 session = Session()
 
+def parse_date(datestr):
+    return datetime.strptime(datestr, '%m/%d/%Y')
+
+
+def parse_time(timestr):
+    return datetime.strptime(timestr, '%I:%M %p')
+
+
 def load_patient():
     for f in glob.glob(os.path.join(data_dir, 'patient?.csv')):
         df = pd.read_csv(f)
         for ix,row in df.iterrows():
-            birthdate = datetime.strptime(row['birthdate'], '%m/%d/%Y')
             pnt = Patient(
                 first_name=row['first_name'],
                 last_name=row['last_name'],
                 id=row['id'],
                 email=row['email'],
                 gender=row['gender'],
-                birthdate=birthdate
+                birthdate=parse_date(row['birthdate'])
             )
             session.add(pnt)
         session.commit()
 
 
+def load_test():
+    for f in glob.glob(os.path.join(data_dir, 'test*.csv')):
+        df = pd.read_csv(f)
+        for ix,row in df.iterrows():
+            test = Test(
+                test_code=row['testCode'],
+                name=row['name'],
+                unit=row['unit']
+            )
+            session.add(test)
+        session.commit()
+
+
+def load_order():
+    tests = ['ldl']
+    for t in tests:
+        for f in glob.glob(os.path.join(data_dir, '{}*.csv'.format(t))):
+            df = pd.read_csv(f)
+            for ix,row in df.iterrows():
+                order = session.query(Order).filter(Order.order_id==row['orderid']).first()
+                if order:
+                    print(order.order_id)
+                test = session.query(Test).filter(Test.test_code==row['testid']).first()
+                orderDateTime = datetime.strptime('{} {}'.format(
+                    row['orderdate'],
+                    row['ordertime']
+                ), '%m/%d/%Y %I:%M %p')
+                recvDateTime = orderDateTime + timedelta(minutes=randint(10,50))
+                reportDateTime = recvDateTime + timedelta(minutes=randint(10,50))
+                lab = Lab(
+                    lab_id=row['labid'],
+                    test_id=test.test_code,
+                    recv_date=recvDateTime.date(),
+                    recv_time=recvDateTime.time(),
+                    report_date=reportDateTime.date(),
+                    report_time=reportDateTime.time(),
+                    results=str(row['results']),
+                )
+                if not order:
+                    order = Order(
+                        order_id=row['orderid'],
+                        orderdate=parse_date(row['orderdate']),
+                        ordertime=parse_time(row['ordertime']),
+                        doctor=row['doctor'],
+                    )
+                order.labs.append(lab)
+                session.add(lab)
+                session.add(order)
+            session.commit()
+
+def add_order_to_patient():
+    patients = list(session.query(Patient))
+    for order in session.query(Order):
+        idx = randint(0,len(patients)-1)
+        patient = patients[idx]
+        patient.orders.append(order)
+        session.add(patient)
+    session.commit()
+
+
 if __name__ == '__main__':
     load_patient()
+    load_test()
+    load_order()
+    add_order_to_patient()
--- simplelis/models.py
+++ simplelis/models.py
@@ -11,8 +11,8 @@ Base = declarative_base()
 class Patient(Base):
     __tablename__ = 'patients'
     id = Column('id', String(16), primary_key=True)
-    first_name = Column('firstName', String(128), nullable=False)
-    last_name = Column('lastName', String(128), nullable=False)
+    first_name = Column('first_name', String(128), nullable=False)
+    last_name = Column('last_name', String(128), nullable=False)
     email = Column('email', String(128), nullable=True)
     gender = Column('gender', String(8), nullable=False)
     birthdate = Column('birthdate', Date(), nullable=False)
@@ -21,27 +21,29 @@ class Patient(Base):
 
 class Order(Base):
     __tablename__ = 'orders'
-    order_id = Column('orderId', String(16), primary_key=True)
-    orderdate = Column('orderDate', Date, nullable=False)
-    ordertime = Column('orderTime', Time, nullable=False)
+    order_id = Column('order_id', String(16), primary_key=True)
+    orderdate = Column('order_date', Date, nullable=False)
+    ordertime = Column('order_time', Time, nullable=False)
     doctor = Column('doctor', String(128))
     labs = relationship('Lab', backref='order')
+    patient_id = Column(String(16), ForeignKey('patients.id'))
 
 
 class Lab(Base):
     __tablename__ = 'labs'
-    lab_id = Column('labId', String(16), primary_key=True)
-    test_id = Column(String(16), ForeignKey('tests.testCode'))
-    recvDate = Column('recvDate', Date, nullable=False)
-    recvTime = Column('recvTime', Time, nullable=False)
-    reportDate = Column('recvDate', Date)
-    reportTime = Column('recvTime', Time)
+    lab_id = Column('lab_id', String(16), primary_key=True)
+    recv_date = Column('recv_date', Date, nullable=False)
+    recv_time = Column('recv_time', Time, nullable=False)
+    report_date = Column('report_date', Date)
+    report_time = Column('report_time', Time)
     results = Column('results', Numeric())
-    test = relationship('Test', backref=backref('lab', uselist=False))
+    test_id = Column(String(16), ForeignKey('tests.test_code'))
+    order_id = Column(String(16), ForeignKey('orders.order_id'))
 
 
 class Test(Base):
     __tablename__ = 'tests'
-    test_code = Column('testCode', String(8), primary_key=True)
+    test_code = Column('test_code', String(8), primary_key=True)
     name = Column('name', String(255), nullable=False)
     unit = Column('unit', String(16))
+    labs = relationship('Lab', backref='test')
